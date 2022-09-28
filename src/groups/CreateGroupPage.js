4f55c4dd3cda24248f00b812fee1c0640d4289f2
Antonio F. Gasco <antoniofgasco@gmail.com>
2022-09-28 11:31:18 +0100

    implement CreateGroupPage component

--- src/groups/CreateGroupPage.js
+++ src/groups/CreateGroupPage.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { postWithCredentials } from '../data';
 
 export const CreateGroupPage = () => {
-  return <div>CreateGroupPage</div>;
+  const [name, setName] = useState('');
+  const history = useHistory();
+
+  const createGroup = async () => {
+    const response = await postWithCredentials('/groups', { name });
+    const { newGroupId } = await response.json();
+
+    history.push(`groups/${newGroupId}`);
+  };
+
+  return (
+    <div className="centered-container">
+      <h1>Create Group</h1>
+      <input
+        type="text"
+        placeholder="Enter Group Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={createGroup}>Create Group</button>
+    </div>
+  );
 };
